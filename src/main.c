503179e4071948e136e211475bca2b9e376f0f7d
Riley Anderson <101540525+rileyandrsn@users.noreply.github.com>
2025-07-28 14:48:22 -0700

    chore: bp code

--- src/main.c
+++ src/main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <pcap.h>
+#include <stdlib.h>
 int main() 
 {
-printf("Hello, world!");
+//char *device; // Name of device (i.e. eth0)
+char errorBuffer[PCAP_ERRBUF_SIZE]; // Size defined as 256 in 'pcap.h'
+
+pcap_if_t *alldevs;
+pcap_if_t *dev;
+
+pcap_findalldevs(&alldevs,errorBuffer);
+printf("%s\n",alldevs->name);
+pcap_if_t *temp = alldevs->next;
+
+while(temp != NULL){
+printf("%s\n",temp->name);
+temp = temp->next;
+}
+pcap_freealldevs(alldevs);
+pcap_freealldevs(temp);
+
+
+
+
+//printf("Device: %s",device);
 }
---
+++ src/sniffer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <pcap.h>
+#include <time.h>
+
+int main(int argc, char *argv[]){
+char errorBuffer[PCAP_ERRBUF_SIZE]; // Size defined as 256 in 'pcap.h'
+
+
+}
